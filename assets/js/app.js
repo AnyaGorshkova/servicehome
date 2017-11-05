2de9bba8fba12af13f0bc27c5432627f399ef1ee
AnyaGorshkova <anua_spb@mail.ru>
2017-11-06 01:22:40 +0300

    Update 2017-11-05T22:22:28.806Z

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/*(function() {
+  'use strict';
+  console.log('Hello');
+})();
+*/
+document.addEventListener("DOMContentLoaded", function(){
+  
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qKGZ1bmN0aW9uKCkge1xuICAndXNlIHN0cmljdCc7XG4gIGNvbnNvbGUubG9nKCdIZWxsbycpO1xufSkoKTtcbiovXG5kb2N1bWVudC5hZGRFdmVudExpc3RlbmVyKFwiRE9NQ29udGVudExvYWRlZFwiLCBmdW5jdGlvbigpe1xuICBcbn0pOyJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
